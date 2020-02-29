8c6656906777f1911139adb4d4941f4671f3c589
Jen Miao <jenmiao@gmail.com>
2020-02-29 05:01:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,27 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+#Below, we are just creating placeholders
   @@count = 0
   @@artists = []
   @@genres = []
+
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+  #Here, we are creating the class variables, @@count, @@genres, @@artists
     @@count += 1
     @@genres << genre
-    @@artists << artist 
+    @@artists << artist
   end
 
-#create class variables
-
-
-
+#Here we are defining a class method, .count
+  def self.count
+    #return number of songs created
+    @@count
+  end 
 
   #def initialize #(can we use initialize more than once in our class?) - no, ended up failing the taking in three arguments test
   # def
